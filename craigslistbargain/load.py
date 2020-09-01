f2d36b338e37c7a32d55645512af8f290690e19e
Siddharth Verma <siddharthverma314@gmail.com>
2020-09-01 02:16:49 -0700

    First sketch of controller

--- craigslistbargain/load.py
+++ craigslistbargain/load.py
@@ -10,65 +10,86 @@ import json
 
 
 FILE_PATH = Path(__file__).absolute().parent
-SCHEMA_PATH = str(FILE_PATH/"data"/"craigslist-schema.json")
-MODEL_PATH = str(FILE_PATH/"checkpoint"/"lf2lf"/"model_best.pt")
-PRICE_TRACKER_PATH = str(FILE_PATH/"price_tracker.pkl")
-DATA_PATH = str(FILE_PATH/"data"/"dev.json")
 
+class Loader:
+    """Load the pre-trained policy pickle files from Codalabs. Assumes
+    the file directory structure in the Dockerfile.
 
-def load_neural_system(use_gpu=False):
-    args = {
-        "model": "lf2lf",
-        "word_vec_size": 300,
-        "dropout": 0.,
-        "encoder_type": "rnn",
-        "decoder_type": "rnn",
-        "context_embedder_type": "mean",
-        "global_attention": "multibank_general",
-        "share_embeddings": False,
-        "share_decoder_embeddings": False,
-        "enc_layers": 1,
-        "copy_attn": False,
-        "dec_layers": 1,
-        "pretrained_wordvec": "",
-        "rnn_size": 300,
-        "rnn_type": "LSTM",
-        "enc_layers": 1,
-        "num_context": 2,
-        "stateful": True,
-        "sample": True,
-        "max_length": 10,
-        "n_best": 1,
-        "batch_size": 128,
-        "optim": "adagrad",
-        "alpha": 0.01,
-        "temperature": 0.5,
-        "epochs": 30,
-        "report_every": 500,
-    }
-    if use_gpu:
-        args.gpuid = 0
-    args = type("args", (), args)
+    """
 
-    with open(PRICE_TRACKER_PATH) as f:
-        price_tracker = pickle.load(f)
-    schema = Schema(SCHEMA_PATH)
+    SCHEMA_PATH = str(FILE_PATH/"data"/"craigslist-schema.json")
+    MODEL_PATH = str(FILE_PATH/"checkpoint"/"lf2lf"/"model_best.pt")
+    PRICE_TRACKER_PATH = str(FILE_PATH/"price_tracker.pkl")
+    DATA_PATH = str(FILE_PATH/"data"/"dev.json")
 
-    system = PytorchNeuralSystem(args, schema, price_tracker, MODEL_PATH, False)
-    return system
+    def __init__(self, use_gpu=False):
+        # make args that are supposed to be passed in by command line arguments
+        args = {
+            "model": "lf2lf",
+            "word_vec_size": 300,
+            "dropout": 0.,
+            "encoder_type": "rnn",
+            "decoder_type": "rnn",
+            "context_embedder_type": "mean",
+            "global_attention": "multibank_general",
+            "share_embeddings": False,
+            "share_decoder_embeddings": False,
+            "enc_layers": 1,
+            "copy_attn": False,
+            "dec_layers": 1,
+            "pretrained_wordvec": "",
+            "rnn_size": 300,
+            "rnn_type": "LSTM",
+            "enc_layers": 1,
+            "num_context": 2,
+            "stateful": True,
+            "sample": True,
+            "max_length": 10,
+            "n_best": 1,
+            "batch_size": 128,
+            "optim": "adagrad",
+            "alpha": 0.01,
+            "temperature": 0.5,
+            "epochs": 30,
+            "report_every": 500,
+        }
+        if use_gpu:
+            args.gpuid = 0
 
+        # HACK: convert args from dict into object. Ex. args["epochs"]
+        # becomes args.epochs
+        args = type("args", (), args)
 
-def get_session_from_uuid(uuid, agent):
-    with open(DATA_PATH) as f:
-        raw = json.load(f)
-    raw = [r["scenario"] for r in raw]
-    schema = Schema(SCHEMA_PATH)
-    scenario_db = ScenarioDB.from_dict(schema, raw, Scenario)
-    kb = scenario_db.get(uuid).get_kb(agent)
-    session = system.new_session(agent, kb)
-    return session
+        # load price tracker
+        with open(self.PRICE_TRACKER_PATH) as f:
+            price_tracker = pickle.load(f)
+
+        # load schema
+        schema = Schema(self.SCHEMA_PATH)
+
+        # load system
+        self.system = PytorchNeuralSystem(
+            args, schema, price_tracker, self.MODEL_PATH, False
+        )
+
+        # load scenario db
+        with open(self.DATA_PATH) as f:
+            raw = json.load(f)
+        raw = [r["scenario"] for r in raw]  # HACK
+        self.scenario_db = ScenarioDB.from_dict(schema, raw, Scenario)
+
+    def from_uuid(self, agent, uuid):
+        """Return a session object given a uuid and agent number"""
+        scenario = self.scenario_db.get(uuid)
+        kb = scenario.get_kb(agent)
+        return scenario, self.system.new_session(agent, kb), kb
 
 
 if __name__ == "__main__":
-    system = load_neural_system()
-    session = get_session_from_uuid("S_To118PXuNicOd8SO", 0)
+    from sessions.cmd_session import CmdSession
+    from core.controller import Controller
+    loader = Loader()
+    scenario, session, kb = loader.from_uuid(0, "S_To118PXuNicOd8SO")
+    cmd_session = CmdSession(1, kb)
+    controller = Controller(scenario, [session, cmd_session])
+    controller.simulate()
